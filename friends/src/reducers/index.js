7916f75e8601da3554cc9a8f9568152f3d8cdfdd
joepak <josephpak2014@gmail.com>
2019-03-07 16:51:52 -0500

    Setting up initialState

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,10 +1,16 @@
 const initialState = {
-    friends: []
+    friends: [],
+    loggingIn: false,
+    fetchingFriends: false,
+    updatingFriends: false,
+    savingFriends: false,
+    deletingFriend: false,
+    error: null
 }
 
 const rootReducer = (state = initialState, action) => {
     switch(action.type) {
-        
+
         default:
             return state;
     }
